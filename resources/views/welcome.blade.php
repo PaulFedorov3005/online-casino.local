d461acc44bf8c028aeff92e82808a02ecf586636
YoungDobrodum <yung.dobrodum@gmail.com>
2022-05-20 00:32:41 +0300

    title

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -38,7 +38,7 @@
     <body>
         <div class="container">
             <div class="content">
-                <div class="title">Laravel 5</div>
+                <div class="title">Всё работает, не переживай</div>
             </div>
         </div>
     </body>
